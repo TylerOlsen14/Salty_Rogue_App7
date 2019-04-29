d9aad7cd26e78991dbd77a80a6ea648abe3eff92
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-04-29 10:47:12 -0600

    Server is working locally, connected to MongoDB

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');// backend framework
+const mongoose = require('mongoose');// ORM to interact with database
+const bodyParser = require('body-parser');// allow requests and get data from body
+
+const app = express();
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected ... '))
+  .catch(err => console.log(err));
+
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server staterd on port ${port}`))
